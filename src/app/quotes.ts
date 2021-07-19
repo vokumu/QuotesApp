0add43ebb978b40907a7b779220b2c23122fc5da
vokumu <victoria.okumu@student.moringaschool.com>
2021-07-19 07:26:27 +0300

    Added constructor and class component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    constructor(public id:number, public quote:string,public author:string,public publisher: string, public upvote:number, public downvote:number)
+    {
+
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:Quotes[]=[
+    new Quotes(1,"this is a quote","Victoria Okuku","Atieno",0,0)
+  ]
   constructor() { }
 
   ngOnInit(): void {
