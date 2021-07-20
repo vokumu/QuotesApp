19ff3ec6d52d7c13cd7152c8f5f1eeac7e3881d0
vokumu <victoria.okumu@student.moringaschool.com>
2021-07-20 18:43:40 +0300

    Created the date-count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FormsModule } from '@angular/forms';
 import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { HighlightDirective } from './highlight.directive';
     QuoteDetailComponent,
     QuoteFormComponent,
     HighlightDirective,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
